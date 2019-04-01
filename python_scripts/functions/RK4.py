334a665df5743c5725f95e98a7f3a0117b047017
Matheus Ladvig <matheus.ladvig@scalian.com>
2019-04-01 20:01:57 +0200

    the new python functions

---
+++ python_scripts/functions/RK4.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 15:23:36 2019
+
+@author: matheus.ladvig
+"""
+from deriv_bt import deriv_bt
+import numpy as np
+
+
+
+def squeeze_python(data):
+    
+    if len(data.shape)==2:
+        return data
+    else:
+        data = np.squeeze(data)
+        return data
+
+
+
+
+def RK4(bt,I,L,C,dt):
+    
+#   Time integration by 4-th order Runge Kutta 
+#   Compute b(t+1)
+#   The sizes of the inputs should be :
+#   - I = [ I(t) I(t+1) I(t+2)] : 3 x m 
+#   - L = [ L(t) L(t+1) L(t+2)] : 3 x m x m
+#   - C : m x m x m
+#   - bt = b(t) : 1 x m 
+#   The result has the size : N x m
+    
+    
+    k1 = deriv_bt(I[0,:][...,np.newaxis],squeeze_python(L[0,:,:]), C, bt)
+    k2 = deriv_bt(I[1,:][...,np.newaxis],squeeze_python(L[1,:,:]), C, bt + k1*dt/2)
+    k3 = deriv_bt(I[2,:][...,np.newaxis],squeeze_python(L[2,:,:]), C, bt + k2*dt/2)
+    k4 = deriv_bt(I[3,:][...,np.newaxis],squeeze_python(L[3,:,:]), C, bt + k3*dt/2)
+    
+    b_tp1 = bt + (dt/3)*(k1/2 + k2 + k3 + k4/2)
+    
+    
+    
+    return b_tp1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ python_scripts/functions/deriv_bt.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 15:29:01 2019
+
+@author: matheus.ladvig
+"""
+import numpy as np
+
+def deriv_bt(I,L,C, bt):
+    
+#   Compute the time derivation of bt
+#   The sizes of the inputs should be :
+#   - I : m
+#   - L : m x m
+#   - C : m x m x m
+#   - bt : N x m 
+#   - ord3 (if exist) : m x m x m x m
+#   - ord4 (if exist) : m x m x m x mx m
+#   The result has the size : N x m
+    
+    N = bt.shape[0]
+    
+    
+#    bt = np.transpose(bt[:,np.newaxis,np.newaxis,:],(1,2,3,0)) #  m x 1 x 1 x N
+    
+    bt = bt.T
+    C = np.multiply(bt,np.transpose(C,(2,0,1)))
+    C = np.transpose(C,(1,2,0))
+    
+    C = np.sum(C,axis=0)
+    
+#    C = squeeze(sum(C,1)); % m x m x N
+#
+#    bt = permute(bt,[1 2 4 3]); % m x 1 x N
+#    
+    
+    C = np.multiply(bt,C)
+    C = np.sum(C,axis=0)
+    
+    C = C.T
+    
+    
+    L = np.multiply(bt,L)
+    L = np.sum(L,axis = 0)
+    
+    db = -(np.tile(I,(1,N)) + L + C).T
+    
+    print(bt)
+    
+    
+    
+    return 2
