b53255ed8e0e9b941ccd7d99af348887c8fc3561
Agustin <agustinmartin.picard@scalian.com>
2019-07-15 12:20:10 +0200

    NOW everything is fixed

--- functions/POD/evol_forward_bt_SSPRK3_MCMC.m
+++ functions/POD/evol_forward_bt_SSPRK3_MCMC.m
@@ -13,18 +13,23 @@ function [bt_evol] = evol_forward_bt_SSPRK3_MCMC(I,L,C, ...
 %
 
 [~ , n , nb_pcl ] = size(bt);
+noises = generate_noises(pchol_cov_noises, n, nb_pcl, dt);
 
 % Do the SSPRK3 integration by steps
-db_m = evolve_sto(pchol_cov_noises, n, nb_pcl, dt, bt);
+db_m = evolve_sto(noises, n, nb_pcl, bt);
 db_m = permute( db_m , [2 1 4 3]);
 k1 = evolve_deter(bt, I, L ,C);
 k1 = permute( k1 , [2 1 4 3]);
 u1 = bt + dt * k1 + db_m;
 k2 = evolve_deter(u1, I, L ,C);
 k2 = permute( k2 , [2 1 4 3]);
+db_m = evolve_sto(noises, n, nb_pcl, u1);
+db_m = permute( db_m , [2 1 4 3]);
 u2 = 3 / 4 * bt + u1 / 4 + dt * k2 / 4 + db_m / 4;
 k3 = evolve_deter(u2, I, L ,C);
 k3 = permute( k3 , [2 1 4 3]);
+db_m = evolve_sto(noises, n, nb_pcl, u2);
+db_m = permute( db_m , [2 1 4 3]);
 
 bt_evol = (bt / 3) + (2 / 3) * (u2 + dt * k3 + db_m);
 bt_evol = permute( bt_evol , [2 1 4 3]);
@@ -43,13 +48,10 @@ db = permute( db , [2 1 4 3]);
 
 end
 
-function db_m = evolve_sto(pchol_cov_noises, n, nb_pcl, dt, bt)
+function db_m = evolve_sto(noises, n, nb_pcl, bt)
 
 bt = permute(bt,[2 1 4 3]); % m x 1 x 1 x nb_pcl
 
-noises = pchol_cov_noises*randn((n+1)*n,nb_pcl)*sqrt(dt);
-noises = permute(noises,[1 3 4 2]); % (n+1)*n x nb_pcl
-clear pchol_cov_noises; % (n+1)*n x 1 x 1 x nb_pcl
 theta_alpha0_dB_t = noises(1:n,1,1,:); % n(i) x 1 x 1 x nb_pcl
 alpha_dB_t = reshape(noises(n+1:end,1,1,:),[n n 1 nb_pcl]); % n(j) x n(i) x 1 x nb_pcl
 clear noises
@@ -76,6 +78,14 @@ L = bsxfun(@times,bt,L); % m x m x 1 x nb_pcl
 L = permute(sum(L,1),[2 3 1 4]); % m x 1 x 1 x nb_pcl
 
 db_fv = - bsxfun(@plus, I, L + C ) ; % m x 1 x 1
+
+end
+
+function noises = generate_noises(pchol_cov_noises, n, nb_pcl, dt)
+
+noises = pchol_cov_noises*randn((n+1)*n,nb_pcl)*sqrt(dt);
+noises = permute(noises,[1 3 4 2]); % (n+1)*n x nb_pcl
+clear pchol_cov_noises; % (n+1)*n x 1 x 1 x nb_pcl
 
 end
 
