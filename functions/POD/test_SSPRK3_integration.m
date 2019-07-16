7c6aa6766879885f1cc44924ef2819edfb03cf8e
Agustin <agustinmartin.picard@scalian.com>
2019-07-16 15:14:45 +0200

    Add comparison between SSPRK3 and Euler Maruyama to finish the debugging phase

--- functions/POD/test_SSPRK3_integration.m
+++ functions/POD/test_SSPRK3_integration.m
@@ -20,8 +20,8 @@ stochastic_evolve = @(A, Xt, C) drift(A, Xt) + martingale(C);
 A = 1.0 .* eye(n_realizations);
 C = 1.0 .* eye(n_realizations);
 X0 = 10.0 .* ones(n_realizations, 1);
-Xt = zeros(n_realizations, T);
-Xt(:, 1) = X0;
+Xt_RK = zeros(n_realizations, T);
+Xt_RK(:, 1) = X0;
 k = 2;
 
 for t = ti : dt: tf
@@ -34,30 +34,30 @@ for t = ti : dt: tf
 %     Xt(:, k) = Xt(:, k - 1) / 3 + (2 / 3) * (u2 + dt * k3);
 %     k = k + 1;
     sigma = martingale(C);
-    k1 = drift(A, Xt(:, k - 1));
+    k1 = drift(A, Xt_RK(:, k - 1));
 %     u1 = Xt(:, k - 1) + dt * k1 + martingale(C);
-    u1 = Xt(:, k - 1) + dt * k1 + sigma;
+    u1 = Xt_RK(:, k - 1) + dt * k1 + sigma;
     k2 = drift(A, u1);
-    u2 = 3 / 4 * Xt(:, k - 1) + (u1 / 4) + (dt * k2 / 4 + sigma / 4);
+    u2 = 3 / 4 * Xt_RK(:, k - 1) + (u1 / 4) + (dt * k2 / 4 + sigma / 4);
 %     u2 = 3 / 4 * Xt(:, k - 1) + (u1 / 4) + (dt * k2 / 4 + martingale(C) / 4);
     k3 = drift(A, u2);
 %     Xt(:, k) = (Xt(:, k - 1) / 3) + (2 / 3) * (u2 + dt * k3 + martingale(C));
-    Xt(:, k) = (Xt(:, k - 1) / 3) + (2 / 3) * (u2 + dt * k3 + sigma);
+    Xt_RK(:, k) = (Xt_RK(:, k - 1) / 3) + (2 / 3) * (u2 + dt * k3 + sigma);
     k = k + 1;
 end
 
 % Through the plot we verify the decay time (normally with a small noise
 % coefficient)
 t = ti : dt : tf;
-figure, plot(t, Xt(:, 1 : end - 1));
+figure, plot(t, Xt_RK(:, 1 : end - 1));
 grid minor, title('Ornstein Uhlenbeck realizations');
 
-Xt_mean = mean(Xt, 1);
+Xt_mean = mean(Xt_RK, 1);
 figure, plot(t, Xt_mean(1 : end - 1));
 grid minor, title('Mean realization')
 
-dXt = diff(Xt);
-noise = sqrt(dt) * randn(length(Xt), 1);
+dXt = diff(Xt_RK);
+noise = sqrt(dt) * randn(length(Xt_RK), 1);
 figure, hold on;
 plot(noise), plot(dXt), grid minor;
 hold off;
@@ -72,7 +72,7 @@ mu = mean(Xt_mean(T / 2 : end));
 for k = 1 : T / 2
     i = 1;
     for j = T / 2 : T - k + 1
-        R_x(:, :, i) = (Xt(:, j) - mu) * (Xt(:, j + k - 1) - mu)';
+        R_x(:, :, i) = (Xt_RK(:, j) - mu) * (Xt_RK(:, j + k - 1) - mu)';
         i = i + 1;
     end
     corr_func(:, :, k) = mean(R_x, 3);
@@ -94,29 +94,53 @@ plot(t, squeeze(corr_func)), plot(t, squeeze(corr_theo)), grid minor;
 title('Theoretical and empirical correlation functions');
 hold off;
 
-%% Simpler test of stochastic component
-n_realizations = 1;
-A = 0.0 .* eye(n_realizations);
+%% Test Euler-Maruyama vs SSPRK3
+clear all, close all, clc;
+
+% Simulation parameters
+ti = 0;
+tf = 1000;
+dt = 0.1;
+n_realizations = 100;
+T = (tf - ti) / dt;
+
+% We'll test with an equation of the form:
+% dX = X dBt
+% Solution should be: X = exp(-A * t) + int_0^t (exp(-A * s) * C dBs)
+drift = @(A, Xt) -A * Xt;
+martingale = @(C) C * sqrt(dt) * randn(n_realizations, 1);
+
+% The Ornstein-Uhlenbeck process has the following form:
+% dX = -A * X dt + C dBt
+% Solution should be: X = exp(-A * t) + int_0^t (exp(-A * s) * C dBs)
+A = 0.1 .* eye(n_realizations);
 C = 1.0 .* eye(n_realizations);
-X0 = ones(n_realizations, 1);
-Xt = zeros(n_realizations, T);
-Xt(:, 1) = X0;
+X0 = 10.0 .* ones(n_realizations, 1);
+Xt_RK = zeros(n_realizations, T);
+Xt_RK(:, 1) = X0;
+Xt_EM = zeros(n_realizations, T);
+Xt_EM(:, 1) = X0;
 k = 2;
 
 for t = ti : dt: tf
     % SSPRK3 integration scheme with small dt for stability
-    k1 = drift(A, Xt(:, k - 1));
-    u1 = Xt(:, k - 1) + dt * k1 + martingale(C);
+    sigma = martingale(C);
+    k1 = drift(A, Xt_RK(:, k - 1));
+    u1 = Xt_RK(:, k - 1) + dt * k1 + sigma;
     k2 = drift(A, u1);
-    u2 = 3 / 4 * Xt(:, k - 1) + (u1 / 4) + (dt * k2 / 4) + martingale(C);
+    u2 = 3 / 4 * Xt_RK(:, k - 1) + (u1 / 4) + (dt * k2 / 4 + sigma / 4);
     k3 = drift(A, u2);
-    Xt(:, k) = (Xt(:, k - 1) / 3) + ((2 / 3) * (u2 + dt * k3)) + martingale(C);
+    Xt_RK(:, k) = (Xt_RK(:, k - 1) / 3) + (2 / 3) * (u2 + dt * k3 + sigma);
+    % Euler Maruyama
+    Xt_EM(:, k) = Xt_EM(:, k - 1) + dt * drift(A, Xt_EM(:, k - 1)) + sigma;
     k = k + 1;
 end
 
-% If it works correctly, the derivative of the brownian motion should be a
-% gaussian with the same variance as before, or close at least
-dXt = diff(Xt);
-noise = randn(length(Xt), 1);
+% Plot the two means
+Xt_RK_mean = mean(Xt_RK, 1);
+Xt_EM_mean = mean(Xt_EM, 1);
+t = ti : dt : tf;
+
 figure, hold on;
-plot(noise), plot(dXt), grid minor;
+plot(t, Xt_RK_mean(1 : end - 1)), plot(t, Xt_EM_mean(1 : end - 1));
+grid minor, title('Euler-Maruyama vs SSPRK3');
