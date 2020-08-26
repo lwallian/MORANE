9c5cdd661135e9a8ff9fd80b2f8c00d38cc98358
LE PAPE Guillaume <guillaume.lepape@scalian.com>
2020-08-26 09:48:21 +0200

    include cpp test script folder in the path

--- init.m
+++ init.m
@@ -6,7 +6,8 @@ dbstop if error;
 current_tests=[ pwd '/current_tests' ];
 fct = genpath([ pwd '/functions' ]);
 mains = [ pwd '/mains'];
+test_Cpp_Matlab = [ pwd '/test_Cpp_Matlab' ];
 addpath(pwd)
 addpath(fct)
-addpath current_tests mains;
-clear fct current_tests mains;
+addpath current_tests mains test_Cpp_Matlab;
+clear fct current_tests mains test_Cpp_Matlab;
